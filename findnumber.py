1062186e8d57e8e4869b168e725bc352af50dcab
deekshith2626 <99652764+deekshith2626@users.noreply.github.com>
2022-02-14 21:25:07 +0530

    Add files via upload

---
+++ findnumber.py
@@ -0,0 +1,8 @@
+a=int(input("ENTER THE NUMBER : "))
+b = 0
+while a>0:
+    b+=1
+    a//=10
+
+
+print("THE GIVEN NUMBER IS :",b)
---
+++ sumofnumber.py
@@ -0,0 +1,9 @@
+a=int(input("ENTER THE NUMBER " ))
+b=0
+while a>0:
+  sum=a%10
+  b=b+sum
+  a=a//10
+
+
+print("THE SUM OF GIVEN NUMBER IS ",b)
